c370997337b4d62092536b9a07ee7b509b18d64e
Angelo Heringa <angeloheringa12@gmail.com>
2020-11-18 14:43:25 +0100

    eerste commit van applicatie                         bevat werkende navigatie

---
+++ app/src/main/java/com/example/safarico/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.safarico;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.eventsFragment, R.id.helpFragment).build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
